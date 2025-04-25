59b06cfc614035ebbe159079f91e8be2f417c4e7
Phil Budne <phil@regressive.org>
2025-04-24 20:50:36 -0400

    Add domain, letsencrypt settings

--- setup.sh
+++ setup.sh
@@ -4,12 +4,22 @@
 
 APP_NAME=dashboard-api
 STATS_SERVICE=ObscureStatsServiceName
+DOMAIN=$APP_NAME.mediacloud.org
+
+if ! grep -sq '^export MCWEB_TOKEN=' key.sh ; then
+    # XXX use a private config repo???
+    echo 'Need key.sh with MCWEB_TOKEN' 1>&2
+    exit 1
+fi
+. ./key.sh
 
 # XXX check first!
 dokku apps:create $APP_NAME
 dokku graphite:link $STATS_SERVICE $APP_NAME
 
-source key.sh
 dokku config:set $APP_NAME MCWEB_TOKEN=$MCWEB_TOKEN
+
+dokku domains:add $APP $DOMAIN
+dokku letsencrypt:enable $APP
 
 git remote add dokku dokku@tarbell:$APP_NAME
